e9019eb02a3941fde2478b070cd923857261792f
maximoskp <maximoskalpap@gmail.com>
2023-11-19 10:56:29 +0200

    fewer

--- World.py
+++ World.py
@@ -61,8 +61,8 @@ class Constants:
     def make_world_constants(self):
         self.world_width = 1920
         self.world_height = 1440
-        self.total_predator_agents = 100
-        self.total_prey_agents = 100
+        self.total_predator_agents = 10
+        self.total_prey_agents = 10
     # end make_world_constants
 
     def make_genetic_constants(self):
